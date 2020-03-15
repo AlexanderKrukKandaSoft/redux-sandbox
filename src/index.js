d676319c1babc6bb30dd8697f89d122340417445
Alexander Kruk <a.kruk@softteco.com>
2020-03-15 12:30:33 +0300

    124. Using React and Redux

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
 import * as actions from './actions';
+import Counter from './counter';
 
 const store = createStore(reducer);
 
-const { dispatch } = store;
+const { dispatch, getState } = store;
 
 const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-document.querySelector('#inc').addEventListener('click', inc);
-document.querySelector('#dec').addEventListener('click', dec);
-document.querySelector('#rnd').addEventListener('click', () => {
-    const payload = Math.floor(Math.random()*10);
-    rnd(payload);
-    });
-
 const update = () => {
-    document.querySelector('#counter').innerHTML = store.getState()
+    ReactDOM.render(
+        <Counter
+            counter={getState()}
+            inc={inc}
+            dec={dec}
+            rnd={() => {
+                const value = Math.floor(Math.random() * 10);
+                rnd(value)
+            }}/>, document.querySelector("#root"));
 }
 
+update();
 store.subscribe(() => {update()})
