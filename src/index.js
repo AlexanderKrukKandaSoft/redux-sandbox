8a8a69dcc3458f656f193b5a2fdda5caa14898b9
Alexander Kruk <a.kruk@softteco.com>
2020-03-15 10:01:34 +0300

    120. Action with params

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const reducer = (state = 0, action) => {
             return state + 1;
         case 'DEC':
             return state - 1;
+        case 'RND':
+            return state + action.payload;
         default:
             return 0;
     }
@@ -15,6 +17,13 @@ const store = createStore(reducer);
 
 document.querySelector('#inc').addEventListener('click', () => { store.dispatch({type: 'INC'})});
 document.querySelector('#dec').addEventListener('click', () => { store.dispatch({type: 'DEC'})});
+document.querySelector('#rnd').addEventListener('click', () => {
+    const payload = Math.floor(Math.random()*10);
+    store.dispatch({
+        type: 'RND',
+        payload
+    }
+    )});
 
 const update = () => {
     document.querySelector('#counter').innerHTML = store.getState()
