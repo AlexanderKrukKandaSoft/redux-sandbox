aeb5b555612d5e973d94c10520b41d72025cc9be
Alexander Kruk <a.kruk@softteco.com>
2020-03-15 08:34:26 +0300

    119. UI for Redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
+import { createStore } from 'redux';
+
 const reducer = (state = 0, action) => {
     switch (action.type) {
         case 'INC':
             return state + 1;
+        case 'DEC':
+            return state - 1;
         default:
             return 0;
     }
 }
 
-let state = reducer(undefined, {type: 'INC'});
-console.log(state);
-state = reducer(state, {type: 'INC'});
-console.log(state);
+const store = createStore(reducer);
+
+document.querySelector('#inc').addEventListener('click', () => { store.dispatch({type: 'INC'})});
+document.querySelector('#dec').addEventListener('click', () => { store.dispatch({type: 'DEC'})});
+
+const update = () => {
+    document.querySelector('#counter').innerHTML = store.getState()
+}
+
+store.subscribe(() => {update()})
